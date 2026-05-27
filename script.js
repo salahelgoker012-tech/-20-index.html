9bde3a44e96fc566118be285c9d3474b617e3a98
salahelgoker012-tech <salahelgoker012@gmail.com>
2026-05-28 00:22:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,76 +1,42 @@
 const correctPassword = "اخوات مهما حصل";
 
 /* الباسورد */
-
 function checkPassword(){
-
   const input = document.getElementById("password").value;
 
   if(input === correctPassword){
-
     document.getElementById("loginPage").classList.remove("active");
-
     document.getElementById("mainPage").classList.add("active");
-
-  }else{
-
+  } else {
     alert("الباسورد غلط يا نجم 😂");
-
   }
-
 }
 
 /* الصور */
-
 const images = [
-
   "Salah1.jpg",
-
   "Salah2.jpg",
-
-  "Salah3.jpg",
-
-  "Salah4.jpg",
-
-  "Salah5.jpg"
+  "Salah3.jpg"
 ];
 
-let current = 0;
-
-/* اظهار الصورة */
+let index = 0;
 
 function showImage(){
-
-  document.getElementById("sliderImage").src = images[current];
-
+  document.getElementById("sliderImage").src = images[index];
 }
 
-/* الصورة اللي بعدها */
-
 function nextImage(){
-
-  current++;
-
-  if(current >= images.length){
-
-    current = 0;
-
+  index++;
+  if(index >= images.length){
+    index = 0;
   }
-
   showImage();
 }
 
-/* الصورة اللي قبلها */
-
 function prevImage(){
-
-  current--;
-
-  if(current < 0){
-
-    current = images.length - 1;
-
+  index--;
+  if(index < 0){
+    index = images.length - 1;
   }
-
   showImage();
 }
